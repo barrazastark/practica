e6d8d5e1d781dc104769b8822cdec20692038935
Jose Barraza <jose.barraza@oracle.com>
2022-04-21 18:05:41 -0500

    Primitivos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 
 import "./reset.css";
+import './primitivos';
+/*
+    JS: client-side, dinamicamente tipado, debilmente tipado
+        asincrono, single-thread, non-blocking, prototype based programming language
+*/
+
+
---
+++ src/primitivos.js
@@ -0,0 +1,49 @@
+// Datos = variables
+
+var pokemonName = "1"; // fin de la instruccion
+var categoria = "Fuego 28 xyz @ _ w.com";
+var img = "http://algo.jpg";
+var age;
+age = 5;
+
+// PRIMITIVOS 
+
+// string - cadena alfanumerica
+//console.log(typeof  pokemonName);
+
+// number (ej: 1,2, 7.4) // NaN "string" / 10
+// ...
+//console.log(typeof age);
+
+// boolean (true, false)
+var charmanderCanEvol = true;
+//console.log(typeof charmanderCanEvol);
+
+// undefined
+var lastEvol;
+//console.log(lastEvol);
+
+// null
+var nextEvol = null;
+//console.log(typeof nextEvol, "*** * ** * * * * ** * ** * * **"); // object
+// error - no fix :(
+
+// Symbol
+var charmanderKey = Symbol("LLAVE SECRETA");
+//console.log(typeof charmanderKey);
+
+
+// Coercion: Mecanisco que usa js para convertir un tipo de dato a otro tipo de dato;
+
+// si hay un string, convertirá a string
+
+// true = 1, false = 0
+
+
+// truthy values y falsy values
+/*
+    Falsy values: "", 0, null, undefined, NaN
+    Truthy values: cualquiera que no sea falsy value
+*/
+
+
